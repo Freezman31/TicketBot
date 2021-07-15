fa330ee12c03638bb631e4c826fd5aaaeffc2ea2
Freezman <raphael.eygonnet@gmail.com>
2021-07-15 17:53:22 +0200

    add real close

--- bin/TicketBot.dart
+++ bin/TicketBot.dart
@@ -8,7 +8,7 @@ Future<void> main(List<String> arguments) async {
   final parser = ArgParser()..addOption('token', abbr: 't', defaultsTo: 'null');
   var result = parser.parse(arguments);
 
-  if(result['token'] == 'null'){
+  if (result['token'] == 'null') {
     print('You must provide a token with -t <token>');
     return;
   }
@@ -21,14 +21,15 @@ Future<void> main(List<String> arguments) async {
   var support = '861307167755993118';
   var thirdhand = '861890244524376084';
   var partner = '861890285855965184';
+  var review = '';
   var archive = '861894489651019787';
 
   bot.onMessageReactionAdded.listen((event) async {
     if (event.messageId == Snowflake(861272991894994955)) {
       var user = await event.user.getOrDownload();
-      var channel = await guild.createChannel(
-              ChannelBuilder('ticket-${user.username}', ChannelType.text))
-          as GuildChannel;
+      var channel = await guild.createChannel(ChannelBuilder.create(
+          name: 'ticket-${user.username}',
+          type: ChannelType.text)) as GuildChannel;
       await channel.editChannelPermissionOverrides(
           PermissionOverrideBuilder(0, Snowflake(781589560944885781))
             ..viewChannel = false);
@@ -40,7 +41,8 @@ Future<void> main(List<String> arguments) async {
       var choices = MultiselectBuilder('select', [
         MultiselectOptionBuilder('Support', 'support', false),
         MultiselectOptionBuilder('Partenariat', 'partner', false),
-        MultiselectOptionBuilder('3e Main', '3e-main', false)
+        MultiselectOptionBuilder('3e Main', '3e-main', false),
+        MultiselectOptionBuilder('Demande de merge (GitHub)', 'review', false)
       ]);
       await bot.httpEndpoints.sendMessage(
           channel.id,
@@ -67,9 +69,10 @@ Future<void> main(List<String> arguments) async {
         var choice = event.interaction.values.first;
         var chana = await event.interaction.channel.download();
         var chan =
-        guild!.channels.firstWhere((element) => element.id == chana.id)
-        as TextGuildChannel;
-        await chan.edit(name: '$choice-${chan.name.split('-')[1]}');
+            guild!.channels.firstWhere((element) => element.id == chana.id)
+                as TextGuildChannel;
+        await chan.edit(
+            ChannelBuilder()..name = '$choice-${chan.name.split('-')[1]}');
         switch (choice) {
           case 'support':
             await changePlace(chan, support, token);
@@ -80,18 +83,20 @@ Future<void> main(List<String> arguments) async {
           case '3e-main':
             await changePlace(chan, thirdhand, token);
             break;
+          case 'review':
+            await changePlace(chan, review, token);
         }
         var msg = await event.interaction.message.download();
         await msg.delete();
         var name = chan.permissionOverrides
             .firstWhere((element) => element.permissions.viewChannel == true)
             .id;
-        chan.permissionOverrides.removeWhere(
-                (element) => element.permissions.viewChannel == true);
+        chan.permissionOverrides
+            .removeWhere((element) => element.permissions.viewChannel == true);
         await chan
             .editChannelPermissionOverrides(PermissionOverrideBuilder(1, name)
-          ..viewChannel = true
-          ..sendMessages = true);
+              ..viewChannel = true
+              ..sendMessages = true);
         await chan.sendMessage(MessageBuilder.embed(EmbedBuilder()
           ..description =
               '**Voici votre ticket !\nVous pouvez faire `/close` pour le fermer !**'
@@ -103,24 +108,24 @@ Future<void> main(List<String> arguments) async {
             hidden: true);
       }
     })
+    ..registerSlashCommand(SlashCommandBuilder('close', 'Ferme le ticket', [],
+        guild: Snowflake(781589560944885781)))
     ..registerSlashCommand(SlashCommandBuilder(
-        'close', 'Close the ticket channel', [],
+        'real-close', 'Ferme le ticket (STAFF ONLY)', [],
         guild: Snowflake(781589560944885781)))
     ..onSlashCommand.listen((event) async {
       var chan = await event.interaction.channel.download();
       if (event.interaction.name == 'close') {
-        var chanName = guild.channels
-            .firstWhere((element) => element.id == chan.id)
-            .name;
+        var chanName =
+            guild.channels.firstWhere((element) => element.id == chan.id).name;
         if (names.contains(chanName.split('-')[0])) {
           var guildChan =
-          await event.interaction.channel.download() as GuildChannel;
+              await event.interaction.channel.download() as GuildChannel;
           var name = guildChan.permissionOverrides
-              .firstWhere(
-                  (element) => element.permissions.viewChannel == true)
+              .firstWhere((element) => element.permissions.viewChannel == true)
               .id;
           guildChan.permissionOverrides.removeWhere(
-                  (element) => element.permissions.viewChannel == true);
+              (element) => element.permissions.viewChannel == true);
           await guildChan.editChannelPermissionOverrides(
               PermissionOverrideBuilder(1, name)..viewChannel = false);
           await event.respond(MessageBuilder.content('Success'));
@@ -129,6 +134,15 @@ Future<void> main(List<String> arguments) async {
           await event.respond(
               MessageBuilder.content('This is not a ticket channel'),
               hidden: true);
+        }
+      } else if (event.interaction.name == 'real-close') {
+        var chanName =
+            guild.channels.firstWhere((element) => element.id == chan.id).name;
+        if (names.contains(chanName.split('-')[0])) {
+          var guildChan =
+              await event.interaction.channel.download() as GuildChannel;
+          await guildChan.delete();
+          await event.respond(MessageBuilder()..content = 'Succesfully deleted');
         }
       }
     })
